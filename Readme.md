d2132b492497e3afc4c71a996a4ad90c56a56444
Marissa Siegel <mrz_87@yahoo.com>
2016-06-11 12:00:56 -0700

    finalized website with working options

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Vacation Suggester
+## JavaScript & jQuery Code Review 2 for Epicodus, June 11, 2016
+
+# Description
+## This is a questionnaire that will provide the output of what vacation to take. You can answer five questions to determine where you should go on your vacation.
+
+# Technology Used
+* HTML
+* CSS
+* JavaScript
+* Bootstrap
+* jQuery
+
+### Copyright owned by Marissa Siegel. For personal use only.
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
-var age = $("input:radio[name=age]:checked").val();
-var beverage = $("#beverage").val();
-var favoriteColor = $("#color").val();
+var florida = function (option1, option2, option3) {
+  return (option1 === 'young' && option2 === 'hot' && option3 === 'three');
+};
+var norway = function (option1, option2, option3) {
+  return (option2 === 'cold');
+};
+var hawaii = function (option1, option2, option3) {
+  return (option1 === 'old' && option2 === 'wet' || option2 === 'hot');
+};
+var none =function () {
+  return "I guess you're not going on vacation.";
+};
 
-alert(you are about to go on the vaccation of your dreams!)
+
+
+$(document).ready(function() {
+  $("form#vacation").submit(function() {
+    event.preventDefault();
+    var option1 = $("input:radio[name='age']:checked").val();
+    var option2 = $("input:radio[name='preference']:checked").val();
+    var option3 = $("input:radio[name='guests']:checked").val();
+    var age = $("input:radio[name=age]:checked").val();
+    var beverage = $("#beverage").val();
+    var favoriteColor = $("#color").val();
+
+    if (florida(option1, option2, option3)) {
+      $("#florida").show()
+      $("#norway").hide()
+      $("#hawaii").hide()
+    } else if (norway(option1, option2, option3)) {
+        $("#florida").hide()
+        $("#norway").show()
+        $("#hawaii").hide()
+    } else if (hawaii(option1, option2, option3)) {
+        $("#florida").hide()
+        $("#norway").hide()
+        $("#hawaii").show()
+    }
+});
+});

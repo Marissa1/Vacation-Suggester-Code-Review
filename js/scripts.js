e40786d507f5cf0f9ac88a5b52c882ea4a35a91f
Marissa Siegel <mrz_87@yahoo.com>
2016-06-11 10:26:59 -0700

    includes 5 questions in html

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var age = $("input:radio[name=age]:checked").val();
+var beverage = $("#beverage").val();
+var favoriteColor = $("#color").val();
+
+alert(you are about to go on the vaccation of your dreams!)
